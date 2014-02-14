427c7a5b6f56ef1a2dc43db7bd042f3ed57505b5
stevebutton <stevebutton@me.com>
2014-02-14 18:45:40 +0100

    enfold plus child

---
+++ www.contentthatmoves.dev/wp-content/themes/enfold-child/js/fdyvideo.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+//pause the video when scroll to div
+function pause() {
+    document.getElementById('myVideo').pause();
+}
+
+
+//play the video when scroll to #top
+function play() {
+    document.getElementById('myVideo').play();
+}
